a82033f229929cc6b74d15b616c097699ecb48b6
RAKOTOVELO Sandarisoa <S133522@apave.com>
2025-03-17 14:38:33 +0100

    ui update

--- frontend/pages/ads.vue
+++ frontend/pages/ads.vue
@@ -21,31 +21,20 @@ onMounted(fetchAds);
 </script>
 
 <template>
-  <div
-    class="min-h-screen flex items-center justify-center bg-gray-900 px-6 py-10"
-  >
-    <div
-      class="w-full max-w-3xl bg-gray-800 p-8 rounded-lg border border-gray-700 shadow-xl"
-    >
+  <div class=" absolute inset-0 min-h-screen flex items-center justify-center bg-gray-900 px-6 py-10">
+    <div class="w-full max-w-3xl bg-gray-800 p-8 rounded-lg border border-gray-700 shadow-xl">
       <h2 class="text-3xl font-semibold text-white mb-6 text-center">
         Annonces Disponibles
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div
-          v-for="(ad, index) in ads"
-          :key="index"
-          class="p-5 bg-gray-900 rounded-lg shadow-lg border border-gray-700 hover:shadow-green-500/50 transition-all hover:scale-105 w-80 h-32 flex flex-col"
-        >
+        <div v-for="(ad, index) in ads" :key="index"
+          class="p-5 bg-gray-900 rounded-lg shadow-lg border border-gray-700 hover:shadow-green-500/50 transition-all hover:scale-105 w-80 h-32 flex flex-col">
           <h3 class="text-lg font-semibold text-white">{{ ad.title }}</h3>
           <p class="text-gray-400 mt-2 flex-grow overflow-hidden">
             {{ ad.description }}
           </p>
-          <img
-            v-if="ad.image"
-            :src="ad.image"
-            alt="Annonce"
-            class="rounded-lg shadow-md w-full h-48 object-cover mt-3"
-          />
+          <img v-if="ad.image" :src="ad.image" alt="Annonce"
+            class="rounded-lg shadow-md w-full h-48 object-cover mt-3" />
         </div>
       </div>
     </div>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,11 +2,8 @@
 
 <template>
   <div
-    class="absolute inset-0 min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-900 via-black to-gray-900 text-white px-4"
-  >
-    <h1
-      class="text-5xl font-extrabold text-green-500 mb-8 text-center drop-shadow-lg"
-    >
+    class="absolute inset-0 min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-900 via-black to-gray-900 text-white px-4">
+    <h1 class="text-5xl font-extrabold text-green-500 mb-8 text-center drop-shadow-lg">
       Bienvenue sur <span class="text-white">BoiteAnnonces</span>
     </h1>
 
@@ -16,10 +13,8 @@
     </p>
 
     <div class="flex flex-wrap justify-center gap-4">
-      <nuxt-link
-        to="/ads"
-        class="px-8 py-3 text-white font-semibold rounded-md shadow-md backdrop-blur-lg bg-gradient-to-r from-green-500 via-green-600 to-green-700 animate-gradient-x hover:shadow-green-500/50 transition-all duration-500 cursor-pointer"
-      >
+      <nuxt-link to="/ads"
+        class="px-8 py-3 text-white font-semibold rounded-md shadow-md backdrop-blur-lg bg-gradient-to-r from-green-500 via-green-600 to-green-700 animate-gradient-x hover:shadow-green-500/50 transition-all duration-500 cursor-pointer">
         Voir les annonces
       </nuxt-link>
     </div>
@@ -31,9 +26,11 @@
   0% {
     background-position: 0% 50%;
   }
+
   50% {
     background-position: 100% 50%;
   }
+
   100% {
     background-position: 0% 50%;
   }
